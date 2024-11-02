777392a3a84aeec7c2f8829a436a6c049cf2bb01
Mihribann7 <m.nosirova@newuu.uz>
2024-11-02 19:48:20 +0500

    yug0101

--- problems.h
+++ problems.h
@@ -1,63 +1,86 @@
-//Problem1
+// Problem 1
 // floorFunction
-template<typename T>
-int floorFucntion (T num) {
-    return num;
+template<typename A>
+int floorFunction(A n)
+{
+    return n/1;
 }
-//ceilingFunction
-template <typename T>
-int ceilingFunction(T num) {
-    if(num==int(num)) {
-        return int(num);
+// ceilingFunction
+template<typename B>
+int ceilingFunction(B n)
+{
+    if(n==int(n))
+    {
+        return int(n);
     }
-    if(num>0) {
-        return int(num)+1;
+    if(n>0)
+    {
+        return int(n)+1;
     }
-    return int(num);
+    return int(n);
 }
-
-
-
-//Problem2
-//swap_values
-void swap_values(double a, double b) {
-    a = a+b;
+// Problem 2x
+// swap_values
+template<typename A>
+void swap_values(A &a, A &b)
+{
+    a=a+b;
     b=a-b;
     a=a-b;
 }
 
-//Problem3
-//multiply
-int multiply(int a, int b) {
-    if (a==0 || b==0)  return 0;
-    return a + multiply(a, b-1);
+// Problem 3
+// multiply
+int multiply(int num1, int num2)
+{
+    if(num2==0)
+    {
+        return 0;
+    }num2--;
+    return num1+multiply(num1,num2);
 }
+// Problem 4
+// digitSum
+int digitSum(int num)
+{
+    int sum = 0;
+    if(num==0)
+    {
+        return 0;
+    }
+    return num%10 + digitSum(num/10);
+};
+// Problem 5
+// decimalToBinary
+int decimalToBinary(int number)
+{
+    if (number == 0)
+    {
+        return 0;
+    }
+    return (number%2)+10* decimalToBinary(number / 2);
+};
 
-//Problem4
-//digitSum
-int digitSum(int n) {
-    if (n==0) return 0;
-    return n%10 + digitSum(n/10);
-}
+// Problem 6
+// midValue
+template<typename T>
+T midValue(T a, T b, T c)
+{
+    if(a>b && b>c  c>b && b>a)
+    {
 
-//Problem5
-//decimalToBinary
-int decimalToBinary(int n) {
-    if (n == 0) return 0;
-
-    return  n % 2+10* decimalToBinary(n / 2);;
-}
-
-//Problem6
-//midvalue
-template <typename T>
-T midValue(T a, T b, T c) {
-    if (a > b && a < c || b>a && a>c)  return a;
-    if (b > a && b < c || a>b && b>>c) return b;
-    return c;
+        return b;
+    }
+    if(a>c && c>b  b>c && c>a)
+    {
+        return c;
+    }
+    if(b>a && a>c || c>a && a>b)
+    {
+        return a;
+    }
 }
 #ifndef PROBLEMS_H
 #define PROBLEMS_H
+
 #endif //PROBLEMS_H
-
-
